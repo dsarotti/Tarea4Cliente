c66eaace652e594a642c74c98aacf63e918e9974
mirianbeta <miyibeta1413@gmail.com>
2024-01-12 12:25:22 +0100

    Elimino la creación de log porque es del Servidor.

--- src/main/java/ClienteTCP.java
+++ src/main/java/ClienteTCP.java
@@ -3,7 +3,6 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ClienteTCP {
-    private static final String LOG_FILE = "FichLog.txt";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
